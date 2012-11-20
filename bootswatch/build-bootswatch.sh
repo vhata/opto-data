5e2df92a9db8666294e24914da437dc75c60685e
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2012-11-20 21:25:59 +0200

    Tweaks for building bootswatch

--- bootswatch/build-bootswatch.sh
+++ bootswatch/build-bootswatch.sh
@@ -6,7 +6,11 @@ patch bootstrap/less/bootstrap.less bootswatch.patch
 cp bootswatch.less variables.less bootstrap/less
 cd bootstrap
 make bootstrap
-mv bootstrap/* ../../static
+for z in css js img ; do
+	mkdir -p ../../static/$z/
+	mv bootstrap/$z/* ../../static/$z/
+	rmdir bootstrap/$z/
+done
 rmdir bootstrap
 rm less/bootswatch.less 
 git checkout -- less/bootstrap.less less/variables.less
